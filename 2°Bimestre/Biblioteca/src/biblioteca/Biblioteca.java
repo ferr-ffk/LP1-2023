9476fc19cf84335148f8f8c956231847629543ec
ferr-ffk <126774900+ferr-ffk@users.noreply.github.com>
2023-05-11 08:35:54 -0300

    Update Biblioteca.java

--- 2°Bimestre/Biblioteca/src/biblioteca/Biblioteca.java
+++ 2°Bimestre/Biblioteca/src/biblioteca/Biblioteca.java
@@ -2,11 +2,12 @@ package biblioteca;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import usuarios.Dono;
 import usuarios.Funcionario;
 import usuarios.Livro;
 
 public class Biblioteca {
+	private Dono dono;
 	private String nome;
 
 	private List<Livro> livros = new ArrayList<>();
@@ -29,7 +30,7 @@ public class Biblioteca {
 	public void addFuncionario(Funcionario funcionario) {
 		funcionarios.add(funcionario);
 	}
-
+	String
 	public void mostraFuncionario() {
 		System.out.println("===Funcionarios===");
 		for (Funcionario funcionario : funcionarios) {
@@ -41,6 +42,11 @@ public class Biblioteca {
 		}
 	}
 
+	public Biblioteca(String nome, Dono dono) {
+		this.dono = dono;
+		this.nome = nome;
+	}
+
 	public String getNome() {
 		return nome;
 	}
@@ -48,4 +54,14 @@ public class Biblioteca {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public String getDono() {
+		return dono.getNome();
+	}
+
+	public void setDono(Dono dono) {
+		this.dono = dono;
+	}
+	
+	
 }

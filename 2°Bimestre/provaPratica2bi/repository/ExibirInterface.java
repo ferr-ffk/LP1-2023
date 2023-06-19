bcc7d91c26a6c4a9b01807a614f0535591723c9e
Fernando Freitas <126774900+ferr-ffk@users.noreply.github.com>
2023-06-19 14:32:29 -0300

    Delete 2°Bimestre/provaPratica2bi directory

--- 2°Bimestre/provaPratica2bi/repository/ExibirInterface.java
+++
@@ -1,8 +0,0 @@
-package provaPratica2bi.repository;
-
-/*O método exibir recebe um booleano que determina se a exibição será completa
-ou reduzida (true para completa e false para reduzida)*/
-
-public interface ExibirInterface {
-	public void exibir(Boolean isCompleta);
-}
